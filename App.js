3e70b3303529794aaf7752a96c4c9c7d47956a33
Rogerio Matoso <rogeriomatoso@yahoo.com.br>
2021-06-14 13:46:58 -0300

    Criado BD,  app medicamentos. Criada connexão e formulario

--- App.js
+++ App.js
@@ -1,12 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import firebase from './src/components/fireBaseConnection';
 
 export default function App() {
+
+  const [medicamento, setMedicamento] = useState('');
+  const [referencia, setReferencia] = useState('');
+  const [principioAtivo, setPrincippioAtivo] = useState('');
+  const [similar, setSimilar] = useState('');
+  const [laboratorio, setLaboratorio] = useState('');
+  const [formaConcentracao, setFormaConcentracao] = useState('');
+
+useEffect(() => {
+  async function dados(){
+    await firebase.database().ref('nome').on('value', (snapshot)=>{
+      //setNome(snapshot.val())
+    })
+  }
+  dados();
+}, [])
+
+async function Inserir(){
+  
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Medicamento:</Text>
+      <TextInput
+      style={styles.input}
+      underlineColorAndroid='transparent'
+      onChangeText = {(texto)=>setMedicamento(texto)}
+      />
+      <Text style={styles.texto}>Referência:</Text>
+      <TextInput
+      style={styles.input}
+      underlineColorAndroid='transparent'
+      onChangeText = {(texto)=>setReferencia(texto)}
+      />
+      <Text style={styles.texto}>Princípio Ativo:</Text>
+      <TextInput
+      style={styles.input}
+      underlineColorAndroid='transparent'
+      onChangeText = {(texto)=>setPrincippioAtivo(texto)}
+      />
+      <Text style={styles.texto}>Medicamento Similar:</Text>
+      <TextInput
+      style={styles.input}
+      underlineColorAndroid='transparent'
+      onChangeText = {(texto)=>setSimilar(texto)}
+      />
+      <Text style={styles.texto}>Laboratório:</Text>
+      <TextInput
+      style={styles.input}
+      underlineColorAndroid='transparent'
+      onChangeText = {(texto)=>setLaboratorio(texto)}
+      />
+      <Text style={styles.texto}>Forma e Concentração:</Text>
+      <TextInput
+      style={styles.input}
+      underlineColorAndroid='transparent'
+      onChangeText = {(texto)=>setFormaConcentracao(texto)}
+      />
+     <TouchableOpacity
+      style={styles.textBtn}
+      onPress={()=>{inserir}}
+
+     
+     >
+       <Text style={{ color: '#fff', fontSize: 18}}>INSERIR</Text>
+       
+     </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +78,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    margin: 10,
+   },
+
+   texto:{
+     fontSize: 18,
+   },
+
+   textBtn:{     
+     padding: 5,
+     backgroundColor: '#48636f',
+     alignItems: 'center',
+     height: 40,    
+     justifyContent: 'center',
+     borderRadius: 15,
+     marginTop: 10,
+     marginBottom: 15,    
+   },
+
+   input:{
+     marginBottom: 10,
+     padding: 10,
+     borderWidth: 1,
+     borderColor: '#121212',
+     borderRadius: 5,
+     height: 35,
+     fontSize: 17
+   },
 });
---
+++ src/components/fireBaseConnection.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+import 'firebase/database';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyB3fQcw0WAAhcNicypXStJcCKFq1V75ew8",
+    authDomain: "appmedicamentos-36c68.firebaseapp.com",
+    projectId: "appmedicamentos-36c68",
+    storageBucket: "appmedicamentos-36c68.appspot.com",
+    messagingSenderId: "250381224248",
+    appId: "1:250381224248:web:5cbb43a8db1f58efe499cd",
+    measurementId: "G-X02TR3JQEF"
+  };
+  // Initialize Firebase
+  if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+  }
+  export default firebase;
